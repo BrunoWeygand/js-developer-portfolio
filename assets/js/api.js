c6982b132f2016013cfc9bc52b0762c08a9c76c8
BrunoWeygand <bruuno.de.oliveira@gmail.com>
2025-02-13 05:39:33 -0300

    Update profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/BrunoWeygand/js-developer-portfolio/refs/heads/main/data/profile.json'
-    const fetching = fetch(url)
+    const fetching = await fetch(url)
     return await fetching.json()
 }
